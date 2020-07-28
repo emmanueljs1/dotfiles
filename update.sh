67ad4f241088fcd6bb870954ba6c200238a37c1e
Emmanuel Suarez <emmanueljs1@gmail.com>
2020-07-28 13:15:18 -0700

    Remove iterm2 config from dotfiles in update.sh

--- update.sh
+++ update.sh
@@ -5,9 +5,3 @@ files="vimrc zshrc"               # dotfiles
 for file in $files; do 
   sed '/.do-not-include/,$d' ~/.$file > $dir/.$file
 done
-
-if [ "$(uname)" == "Darwin" ]; then
-    if [ -d /Applications/iTerm.app ]; then
-        scp ~/Library/Preferences/com.googlecode.iterm2.plist $dir/com.googlecode.iterm2.plist
-    fi
-fi
